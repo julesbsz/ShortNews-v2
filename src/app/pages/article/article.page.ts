3ee856bca92a901ebb6e8df1bece33a42d41b96a
Jules Bousrez <38622433+julesbsz@users.noreply.github.com>
2023-03-01 10:13:34 -0500

    Share Article Functionality

--- src/app/pages/article/article.page.ts
+++ src/app/pages/article/article.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Browser } from '@capacitor/browser';
-import { NavController } from '@ionic/angular';
+import { Share } from '@capacitor/share';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-article',
@@ -10,7 +11,11 @@ import { NavController } from '@ionic/angular';
 })
 export class ArticlePage implements OnInit {
   data: any;
-  constructor(private router: Router, private navController: NavController) {}
+  constructor(
+    private router: Router,
+    private navController: NavController,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
     const routerState = this.router.getCurrentNavigation()?.extras.state;
@@ -34,5 +39,30 @@ export class ArticlePage implements OnInit {
 
   async openBrowser() {
     await Browser.open({ url: this.data.url });
+  }
+
+  async shareArticle() {
+    const result = await Share.canShare();
+    if (result.value) {
+      await Share.share({
+        title: this.data.title,
+        text: this.data.description,
+        url: this.data.url,
+        dialogTitle: 'Share the article',
+      });
+    } else {
+      this.presentToast();
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Oops, unable to share article :/',
+      duration: 1500,
+      position: 'bottom',
+      color: 'danger',
+    });
+
+    await toast.present();
   }
 }
